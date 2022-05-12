498ca009785bf777766835002501d5dc882820d2
Jonasthj <jonasthenderupjensen@gmail.com>
2022-05-12 16:27:16 +0200

    Quiz colors

--- Multiplayer Lobby/Assets/Scripts/AnswerManager.cs
+++ Multiplayer Lobby/Assets/Scripts/AnswerManager.cs
@@ -1,14 +1,16 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class AnswerManager : MonoBehaviour
 {
     public void OnClickAnswerBtn()
     {
         var go = EventSystem.current.currentSelectedGameObject;
-        var answerText = go.GetComponent<TMP_Text>().text;
-
+        var btnImage = go.GetComponent<Image>();
+        
+        btnImage.color = go.GetComponent<AnswerButton>().isCorrect ? Color.green : Color.red;
         
     }
 }
