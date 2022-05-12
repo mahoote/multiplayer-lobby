b144343e9bb138497b25b1baa39ad0fcacdfa61e
Martin Teigen <martin@teigen.biz>
2022-05-12 14:38:42 +0200

    Setup quiz page

--- Multiplayer Lobby/Assets/Scripts/Photon/PlayerSpawner.cs
+++ Multiplayer Lobby/Assets/Scripts/Photon/PlayerSpawner.cs
@@ -1,18 +1,23 @@
 using Photon.Pun;
 using Saving;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Photon
 {
     public class PlayerSpawner : MonoBehaviour
     {
-        [SerializeField] private GameObject[] playerPrefabs;
+        [SerializeField] private TMP_Text playerNameTxt;
+        [SerializeField] private Image playerAvatarImage;
+        [SerializeField] private TMP_Text roomCodeTxt;
+
+        [SerializeField] private Sprite[] avatarSprites;
+        
         
         private void Start()
         {
-            GameObject playerToSpawn = playerPrefabs[(int) PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]];
-            PhotonNetwork.Instantiate(playerToSpawn.name, Vector3.zero, Quaternion.identity);
-
             PlayerDetails.SetPlayerDetails(
                 PhotonNetwork.LocalPlayer.ActorNumber,
                 PhotonNetwork.LocalPlayer.NickName,
@@ -20,6 +25,15 @@ namespace Photon
                 PhotonNetwork.CurrentRoom.Name);
         
             PlayerDetails.LogPlayerDetails();
+
+            playerAvatarImage.sprite = avatarSprites[PlayerPrefs.GetInt("avatarIndex")];
+            playerNameTxt.text = PlayerPrefs.GetString("nickname");
+            roomCodeTxt.text = $"Room code: {PlayerPrefs.GetString("sessionId")}";
+        }
+
+        public void OnClickTakeQuiz()
+        {
+            SceneManager.LoadScene("Quiz");
         }
     }
 }
